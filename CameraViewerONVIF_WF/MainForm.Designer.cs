e6cd0d3d540c22795458df2fce50fc405a669e48
ALEXANDROV ALEXEJ <95049885+alexsamara13@users.noreply.github.com>
2022-11-08 23:31:39 +0400

    Add files via upload

---
+++ CameraViewerONVIF_WF/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Camera_Viewer_Connect_ONVIF_WF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.btn_Disconnect = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.videoViewerWF1 = new Ozeki.Media.VideoViewerWF();
+            this.SuspendLayout();
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Connect.Location = new System.Drawing.Point(567, 1);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(71, 19);
+            this.btn_Connect.TabIndex = 6;
+            this.btn_Connect.Text = "Connect";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // btn_Disconnect
+            // 
+            this.btn_Disconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Disconnect.Enabled = false;
+            this.btn_Disconnect.Location = new System.Drawing.Point(567, 21);
+            this.btn_Disconnect.Name = "btn_Disconnect";
+            this.btn_Disconnect.Size = new System.Drawing.Size(71, 19);
+            this.btn_Disconnect.TabIndex = 7;
+            this.btn_Disconnect.Text = "Disconnect";
+            this.btn_Disconnect.UseVisualStyleBackColor = true;
+            this.btn_Disconnect.Click += new System.EventHandler(this.btn_Disconnect_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 308);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 13);
+            this.statusLabel.TabIndex = 8;
+            // 
+            // videoViewerWF1
+            // 
+            this.videoViewerWF1.BackColor = System.Drawing.Color.Black;
+            this.videoViewerWF1.ContextMenuEnabled = true;
+            this.videoViewerWF1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoViewerWF1.FlipMode = Ozeki.Media.FlipMode.None;
+            this.videoViewerWF1.FrameStretch = Ozeki.Media.FrameStretch.Uniform;
+            this.videoViewerWF1.FullScreenEnabled = true;
+            this.videoViewerWF1.Location = new System.Drawing.Point(0, 0);
+            this.videoViewerWF1.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.videoViewerWF1.Name = "videoViewerWF1";
+            this.videoViewerWF1.RotateAngle = 0;
+            this.videoViewerWF1.Size = new System.Drawing.Size(638, 362);
+            this.videoViewerWF1.TabIndex = 9;
+            this.videoViewerWF1.Text = "videoViewerWF1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(638, 362);
+            this.Controls.Add(this.btn_Disconnect);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.videoViewerWF1);
+            this.Controls.Add(this.statusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximumSize = new System.Drawing.Size(1291, 757);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Camera 192.168.1.40";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Button btn_Disconnect;
+        private System.Windows.Forms.Label statusLabel;
+        private Ozeki.Media.VideoViewerWF videoViewerWF1;
+    }
+}
---
+++ CameraViewerONVIF_WF/MainForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+using Ozeki.Camera;
+using Ozeki.Media;
+
+namespace Camera_Viewer_Connect_ONVIF_WF
+{
+    public partial class MainForm : Form
+    {
+        private OzekiCamera _camera;
+        private DrawingImageProvider _imageProvider;
+        private MediaConnector _connector;
+        private CameraURLBuilderWF _myCameraUrlBuilder;
+        //var url = "192.168.1.40:80;Username = admin;Password = admin;Transport = UDP; ;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _connector = new MediaConnector();
+            _imageProvider = new DrawingImageProvider();
+            // Create video viewer UI control
+            _myCameraUrlBuilder = new CameraURLBuilderWF();
+            // Bind the camera image to the UI control
+            videoViewerWF1.SetImageProvider(_imageProvider);
+        }
+
+        // Connect camera video channel to image provider and start
+        //private void connectBtn_Click(object sender, EventArgs e)
+        //{
+        //    var result = _myCameraUrlBuilder.ShowDialog();
+
+        //    //if (result == DialogResult.OK)
+        //   // {
+        //       // tb_cameraUrl.Text = _myCameraUrlBuilder.CameraURL;
+
+        //        btn_Connect.Enabled = true;
+        //   // }
+        //}
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+           // tb_cameraUrl.Text = "192.168.1.40:80;Username = admin;Password = admin;Transport = UDP;" ;
+            if (_camera != null)
+            {
+                _camera.CameraStateChanged -= _camera_CameraStateChanged;
+                _camera.Disconnect();
+                _connector.Disconnect(_camera.VideoChannel, _imageProvider);
+                _camera.Dispose();
+                _camera = null;
+            }
+
+            btn_Connect.Enabled = false;
+
+            var url = "192.168.1.40:80;Username = admin;Password = admin;Transport = UDP;";
+
+            _camera = new OzekiCamera(url);
+      
+            _camera.CameraStateChanged += _camera_CameraStateChanged;
+        
+            _connector.Connect(_camera.VideoChannel, _imageProvider);
+
+            _camera.Start();
+            videoViewerWF1.Start();
+        }
+
+        void _camera_CameraStateChanged(object sender, CameraStateEventArgs e)
+        {
+            InvokeThread(() =>
+            {
+                StateChanged(e.State);
+
+                if(e.State == CameraState.Streaming)
+                    Streaming();
+
+                if(e.State == CameraState.Disconnected)
+                    Disconnect();
+            });
+        }
+
+        private void StateChanged(CameraState state)
+        {
+            statusLabel.Text = state.ToString();
+        }
+
+        private void Disconnect()
+        {
+            btn_Connect.Enabled = true;
+            btn_Disconnect.Enabled = false;
+        }
+
+        private void Streaming()
+        {
+            btn_Disconnect.Enabled = true;
+        }
+
+        private void btn_Disconnect_Click(object sender, EventArgs e)
+        {
+            if (_camera == null) return;
+
+            _camera.Disconnect();
+            _connector.Disconnect(_camera.VideoChannel, _imageProvider);
+            _camera = null;
+        }
+
+        void InvokeThread(Action action)
+        {
+            BeginInvoke(action);
+        }
+    }
+}
